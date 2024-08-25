e87b18106866abeffa4af2dd53c6bc323208762b
Dragon <66880163+Dragon75240@users.noreply.github.com>
2024-08-25 17:32:34 -0600

    i forgot the docs 😭😭

--- src/docs/assets/navigation.js
+++
@@ -1,1 +0,0 @@
-window.navigationData = "data:application/octet-stream;base64,H4sIAAAAAAAACouuVipJrShRslJySU1LLSpKTQlKTc8sLkktUtJRKkgsyVCyUkrOSSwuTi3WR1ehl1GSm6Oko5SdmZeiZGVoZFGrAzcNoqaoEtMUmAw+3Z4wRf5JWanJJQhDMvNKUovSEpNTi/XR1KAaZ2RqVhsLAGg6kYrcAAAA"
