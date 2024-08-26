526865762ef59259ac63c9d4deed64ed6aa3812c
Dragon <66880163+Dragon75240@users.noreply.github.com>
2024-08-25 18:06:55 -0600

    Postcommit

--- test/src/server/main.server.ts
+++
@@ -1,9 +0,0 @@
-import { DeferredRegister, Registry } from "shared/DeferredRegister";
-import { Item } from "shared/Types";
-
-let StockRegistries = {
-    defaultItem: new Registry("default")
-};
-
-let NewRegistry = new DeferredRegister<Item>(StockRegistries.defaultItem);
-let NewRegistryObject = NewRegistry.register("new_item", new Item("Item", {}));
